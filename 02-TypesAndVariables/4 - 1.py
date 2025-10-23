12d61cb5e34fd118f838805af85c445046a7d170
AsselNurmakhanova <asyanur6188@gmail.com>
2025-10-23 19:58:22 +0200

    5

--- 02-TypesAndVariables/4 - 1.py
+++ 02-TypesAndVariables/4 - 1.py
@@ -1,2 +1,5 @@
+year = int(input("Write your age: "))
+height = int(input("Write you height: "))
+future = year + 6
 name = "Adam"
-print(f"My name is {name}.")
+print(f"My name is {name}. I am {year} years old, and my height is {height} cm. In 6 years I will be {future} years old.")
---
+++ 02-TypesAndVariables/5 - 3.py
@@ -0,0 +1,6 @@
+a = int(input("a = "))
+b = int(input("b = "))
+c = int(input("c = "))
+volume = a * b * c
+surface_area = 2 * (a * b + a * c + b *c)
+print(f"volume = {volume} and surface area = {surface_area}")
