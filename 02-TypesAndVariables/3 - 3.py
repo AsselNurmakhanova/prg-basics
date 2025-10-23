78eff167d93bf0429544daa183c2940e5a13026a
AsselNurmakhanova <asyanur6188@gmail.com>
2025-10-23 19:31:41 +0200

    3

---
+++ 02-TypesAndVariables/3 - 3.py
@@ -0,0 +1,8 @@
+x = 7
+y = 34
+z = 0 # additional, auxiliary variable
+print("Before swapping: x=", x, "y=", y)
+z = y
+y = x
+x = z 
+print("After swapping: x=", x, "y=", y)
---
+++ 02-TypesAndVariables/3 - 6.py
@@ -0,0 +1,4 @@
+import math
+h = int(input("Write a height in metres: "))
+d = 3.57 * math.sqrt(h)
+print("Distance to the horizon in kilometres: ", d)
