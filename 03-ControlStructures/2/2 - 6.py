fd83ebcf0646d24bfe9bdf279e139eb67efc6cef
AsselNurmakhanova <asyanur6188@gmail.com>
2025-10-30 18:04:50 +0100

    n

---
+++ 03-ControlStructures/2/2 - 6.py
@@ -0,0 +1,8 @@
+number = int(input("Write a number: "))
+if number > 0:
+    print(f"Number {number} is positive")
+elif number == 0:
+    print("Number is 0")
+else:
+    print(f"Number {number} is negative")
+    
---
+++ 03-ControlStructures/3/3 - 3.py
@@ -0,0 +1,5 @@
+age = int(input('Enter your age: '))
+if age < 18 or age > 65:
+    print("discount")
+else:
+    print("no discount")
